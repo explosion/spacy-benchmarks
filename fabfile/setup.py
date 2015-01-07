6c5d38b7afe3c529ab57b8bc32cddef1e51d2c5e
Matthew Honnibal <honnibal@gmail.com>
2015-01-07 21:57:41 +1100

    * Extend setup script, having it install zpar. Add stubs for stanford and turbo parser.

--- fabfile/setup.py
+++ fabfile/setup.py
@@ -3,6 +3,7 @@ from fabric.api import *
 from os.path import exists as file_exists
 from fabtools.python import virtualenv
 from os import path
+import os
 from ._cfg import VENV_DIR
 
 
@@ -10,12 +11,45 @@ PWD = path.dirname(__file__)
 
 
 @task(default=True)
-def env(lang="python2.7"):
+def init(lang="python2.7"):
+    make_env(lang)
+    install_spacy()
+    install_nltk()
+    install_zpar()
+    install_stanford()
+    install_turbo()
+    if not path.exists('data'):
+        os.mkdir(path.join('data'))
+
+
+@task
+def make_env(lang="python2.7"):
     if file_exists(VENV_DIR):
         local('rm -rf %s' % VENV_DIR)
     local('virtualenv -p %s %s' % (lang, VENV_DIR))
     with virtualenv(VENV_DIR):
         local('pip install setuptools==9.0')
         local('pip install plac')
+
+
+@task
+def install_spacy():
+    with virtualenv(VENV_DIR):
         local('pip install spacy')
+
+
+@task
+def install_nltk():
+    with virtualenv(VENV_DIR):
+        local('pip install numpy')
         local('pip install nltk')
+
+
+@task
+def install_zpar():
+    local('pip install python-zpar')
+
+
+@task
+def install_stanford():
+    pass
