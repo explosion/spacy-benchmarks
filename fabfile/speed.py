b5d801e58d9b79c9f5fb025f127acdb7f4977098
Matthew Honnibal <honnibal@gmail.com>
2015-01-10 08:49:05 +1100

    * Slightly hacky speed benchmark runners. needs refactor.

--- fabfile/speed.py
+++ fabfile/speed.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 from __future__ import division
+import platform
 
 from fabric.api import *
 from fabtools.python import virtualenv
+
 
 from _cfg import VENV_DIR, GIGA_LOC
 
@@ -29,18 +31,10 @@ class Timer(object):
         return '%s\t%.2f ms / doc' % (self.name, self.msecs / self.n)
 
 
-# The code here is quite repetitive, by choice.
-# Because these scripts all just dispatch a bunch of jobs, I find it easier to
-# read the list of commands in sequence, instead of following wrappers upon
-# wrappers.
-# This also makes it easier to understand any one function independently, and to
-# update them independently in ad hoc ways.
-
-
 @task
 def tok(n=1000):
     n = int(n)
-    with virtualenv(VENV_DIR):
+    with virtualenv(str(VENV_DIR)):
         with Timer('noop', n) as noop_time:
             local('time bin/run_noop.py %s %d' % (GIGA_LOC, n))
         with Timer('split', n) as split_time:
@@ -58,12 +52,18 @@ def tok(n=1000):
 
 
 @task
-def tag(n=1000, to_run='snz'):
+def tag(n=1000, to_run='csnz'):
     do_spacy = 's' in to_run
     do_zpar = 'z' in to_run
     do_nltk = 'n' in to_run
+    do_corenlp = 'c' in to_run
+    if platform.system() != 'Darwin':
+        do_zpar = False
     n = int(n)
-    with virtualenv(VENV_DIR):
+    with virtualenv(str(VENV_DIR)):
+        with Timer('corenlp', n) as corenlp_time:
+            if do_corenlp:
+                local('time bin/run_stanford.py -t -n %d %s' % (n, GIGA_LOC))
         with Timer('spacy', n) as spacy_time:
             if do_spacy:
                 local('time bin/run_spacy.py -t -n %d %s' % (n, GIGA_LOC))
@@ -75,16 +75,24 @@ def tag(n=1000, to_run='snz'):
                 local('time bin/run_zpar.py -t -n %d %s' % (n, GIGA_LOC))
         print("Milliseconds per document of the taggers:")
         print(spacy_time)
+        print(corenlp_time)
         print(zpar_time)
         print(nltk_time)
 
 
 @task
-def parse(n=1000):
+def parse(n=1000, to_run='csnz'):
     do_spacy = 's' in to_run
     do_zpar = 'z' in to_run
+    do_nltk = 'n' in to_run
+    do_corenlp = 'c' in to_run
+    if platform.system() != 'Darwin':
+        do_zpar = False
     n = int(n)
-    with virtualenv(VENV_DIR):
+    with virtualenv(str(VENV_DIR)):
+        with Timer('corenlp', n) as corenlp_time:
+            if do_corenlp:
+                local('time bin/run_stanford.py -t -p -n %d %s' % (n, GIGA_LOC))
         with Timer('spacy', n) as spacy_time:
             if do_spacy:
                 local('time bin/run_spacy.py -t -p -n %d %s' % (n, GIGA_LOC))
@@ -93,4 +101,5 @@ def parse(n=1000):
                 local('time bin/run_zpar.py -t -p -n %d %s' % (n, GIGA_LOC))
         print("Milliseconds per document of the parsers:")
         print(spacy_time)
+        print(corenlp_time)
         print(zpar_time)
