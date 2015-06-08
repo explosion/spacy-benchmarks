5d36e3e7609d72540cfc04cece9aabd5f00d5571
Matthew Honnibal <honnibal@gmail.com>
2015-06-08 03:53:35 +0200

    * Add output to run_spacy

--- bin/run_spacy.py
+++ bin/run_spacy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import plac
+import codecs
 
 from spacy.en import English
 from lib.corpus import Gigaword
@@ -14,8 +15,15 @@ from lib.corpus import Gigaword
 def main(giga_db_loc, n_docs, pos_tag=False, parse=False):
     docs = Gigaword(giga_db_loc, limit=n_docs)
     nlp = English()
-    for doc in docs:
+    out_dir = '/tmp/spacy_out'
+    if path.exists(out_dir):
+        shutil.rmtree(out_dir)
+    for i, doc in enumerate(docs):
         tokens = nlp(doc, tag=pos_tag, parse=parse)
+        with codecs.open(path.join(out_dir, '%d.txt' % i), 'w', 'utf8') as file_:
+            for sent in tokens.sents:
+                for word in sent:
+                    file_.write('%d\t%s\t%s\t%s\t%d' % (word.orth_, word.tag_, word.head.i, word.dep_))
 
 
 if __name__ == '__main__':
